43800a18ea438aec0626dd00d55aab9d9b2f7e94
Jining Liu <hello@jining.dev>
2025-12-24 17:29:22 -0600

    constexpr color fix

--- src/color.h
+++ src/color.h
@@ -10,14 +10,11 @@
 /// @brief Provides predefined color constants for LED displays.
 namespace color
 {
-  constexpr CRGB kOff = CHSV(0, 0, 0); ///< LED off (black).
+  constexpr CRGB kOff = CRGB(0, 0, 0); ///< LED off (black).
 
-  constexpr CRGB kArtifactPurple =
-      CHSV(276 / 360.0 * 255, 95 / 100.0 * 255, 94 / 100.0 * 255); ///< DECODE artifact purple.
-  constexpr CRGB kArtifactGreen =
-      CHSV(129 / 360.0 * 255, 91 / 100.0 * 255, 78 / 100.0 * 255); ///< DECODE artifact green.
+  constexpr CRGB kArtifactPurple = CRGB(163, 12, 226); ///< DECODE artifact purple.
+  constexpr CRGB kArtifactGreen = CRGB(85, 198, 92);   ///< DECODE artifact green.
 
-  constexpr CRGB kSlingshotPurple =
-      CHSV(255 / 360.0 * 255, 67 / 100.0 * 255, 64 / 100.0 * 255); ///< Team Slingshot purple.
+  constexpr CRGB kSlingshotPurple = CRGB(164, 53, 163); ///< 20240 Slingshot purple.
 
 } // namespace color
